c8d93bd9aeca992317dba8ce2bf020748d754036
Nichole Carlson <nichole0077@gmail.com>
2015-05-17 18:40:44 -0500

    Update

---
+++ QueryRunnerWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QueryRunnerWebApp.Startup))]
+namespace QueryRunnerWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
